9ed1094cfe76e374be6a0ba1f1f65490320cb5f6
marcoh1012 <33380761+marcoh1012@users.noreply.github.com>
2020-05-11 14:45:28 -0500

    clean up and slight code change

--- connect4.js
+++ connect4.js
@@ -8,8 +8,8 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
-let currPlayer = "p1"; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = "1"; // active player: 1 or 2
+let board // array of rows, each row is array of cells  (board[y][x])
 let allowClick = true;
 const newGameButton = document.querySelector("#newGameButton");
 newGameButton.addEventListener("click", newGame);
@@ -20,261 +20,257 @@ const displayPlayer = document.querySelector("#currentPlayer");
  */
 
 function makeBoard() {
-  board = [];
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-  for (let col = 0; col < HEIGHT; col++) {
-    board.push([]);
-    for (let row = 0; row < WIDTH; row++) {
-      board[col].push([]);
+    board = [];
+    //TODO: set "board" to empty HEIGHT x WIDTH matrix array
+
+    for (let y = 0; y < HEIGHT; y++) {
+        board.push(Array.from({ length: WIDTH }));
     }
-  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  displayPlayer.innerText = "1";
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-  const htmlBoard = document.querySelector("#board");
-  // TODO: add comment for this code
+    displayPlayer.innerText = "1";
+    // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+    const htmlBoard = document.querySelector("#board");
+    // TODO: add comment for this code
 
-  //creates the top 7 blocks with click listeners
-  //creates an element for each box with a listener
-  //appends the elements from left to right with the idx stored as the id
-  //appends the top column to the dom
-  const top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.setAttribute("class", "player1Turn");
-  top.addEventListener("click", handleClick);
+    //creates the top 7 blocks with click listeners
+    //creates an element for each box with a listener
+    //appends the elements from left to right with the idx stored as the id
+    //appends the top column to the dom
+    const top = document.createElement("tr");
+    top.setAttribute("id", "column-top");
+    top.setAttribute("class", "player1Turn");
+    top.addEventListener("click", handleClick);
 
-  for (let x = 0; x < WIDTH; x++) {
-    const headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
-    headCell.setAttribute("class", "placeHere");
-    headCell.innerText = "Drop Here";
-    top.append(headCell);
-  }
-  htmlBoard.append(top);
-
-  // TODO: add comment for this code
-
-  //creates the board that will display the game pieces.
-  //creates a row element
-  //creates each cell based on the width
-  //appends each cell with id x an y that shows where the cell is ocated on the board. to the row
-  //appends the row to the board game
-  //loops the same as the height to fill up the board
-  for (let y = 0; y < HEIGHT; y++) {
-    const row = document.createElement("tr");
     for (let x = 0; x < WIDTH; x++) {
-      const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
-      cell.setAttribute("class", "pieceContainer");
-      row.append(cell);
+        const headCell = document.createElement("td");
+        headCell.setAttribute("id", x);
+        headCell.setAttribute("class", "placeHere");
+        headCell.innerHTML = "&#8659;";
+        top.append(headCell);
     }
-    htmlBoard.append(row);
-  }
+    htmlBoard.append(top);
+
+    // TODO: add comment for this code
+
+    //creates the board that will display the game pieces.
+    //creates a row element
+    //creates each cell based on the width
+    //appends each cell with id x an y that shows where the cell is ocated on the board. to the row
+    //appends the row to the board game
+    //loops the same as the height to fill up the board
+    for (let y = 0; y < HEIGHT; y++) {
+        const row = document.createElement("tr");
+        for (let x = 0; x < WIDTH; x++) {
+            const cell = document.createElement("td");
+            cell.setAttribute("id", `${y}-${x}`);
+            cell.setAttribute("class", "pieceContainer");
+            row.append(cell);
+        }
+        htmlBoard.append(row);
+    }
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  for (let y = HEIGHT - 1; y >= 0; y--) {
-    const cell = document.getElementById(`${y}-${x}`);
-    if (!cell.innerHTML) {
-      return y;
+    // TODO: write the real version of this, rather than always returning 0
+    for (let y = HEIGHT - 1; y >= 0; y--) {
+        if (!board[y][x]) {
+            return y;
+        }
     }
-  }
-  return null;
+    return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
-function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
-  const chipCell = document.getElementById(`${y}-${x}`);
-  const chip = document.createElement("div");
-  let topPosition = (y + 1) * 9.75;
-  chip.classList.add("piece", currPlayer);
-  chip.setAttribute("style", `position:relative; bottom:${topPosition}vh`);
-  chipCell.append(chip);
-  const animate = setInterval(() => {
-    if (topPosition > 0) {
-      topPosition = topPosition - 0.5;
-      chip.style.bottom = `${topPosition}vh`;
-    } else {
-      clearInterval(animate);
-      nextTurn(y, x);
-    }
-  }, 5);
+function placeInTable(y, x, callback) {
+    // TODO: make a div and insert into correct table cell
+    const chipCell = document.getElementById(`${y}-${x}`);
+    const chip = document.createElement("div");
+
+    let topPosition = (y * 72) + 84;
+    chip.setAttribute("style", `transform: translateY(-${topPosition}px)`);
+    chip.classList.add("piece", `p${currPlayer}`);
+    chipCell.append(chip);
+
+
+    chip.addEventListener('animationend', () => {
+        callback();
+    });
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  allowClick = false;
-  alert(msg);
-  // TODO: pop up alert message
+    allowClick = false;
+    alert(msg);
+    // TODO: pop up alert message
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
-  if (!allowClick) {
-    return;
-  }
-  // get x from ID of clicked cell
-  let x = +evt.target.id;
-  allowClick = false;
-  // get next spot in column (if none, ignore click)
-  let y = findSpotForCol(x);
-  if (y === null) {
-    allowClick = true;
-    return;
-  }
+    if (!allowClick) {
+        return;
+    }
+    // get x from ID of clicked cell
+    let x = +evt.target.id;
+    allowClick = false;
+    // get next spot in column (if none, ignore click)
+    let y = findSpotForCol(x);
+    if (y === null) {
+        allowClick = true;
+        return;
+    }
 
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  board[y][x] = currPlayer;
-  placeInTable(y, x);
-}
 
-/**go to next turn after animation is complete */
-function nextTurn(y, x) {
-  allowClick = true;
-  currPlayer === "p1"
-    ? (displayPlayer.innerText = "2")
-    : (displayPlayer.innerText = "1");
-  // check for win
-  if (checkForWin(y, x)) {
-    return endGame(`Player ${currPlayer.charAt(1)} won!`);
-  }
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  if (board.every((cell) => cell === "")) {
-    endGame(`Tie Game! Start New Game`);
-    console.log("tie");
-  }
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
-  const htmlBoard = document.querySelector("#column-top");
-  currPlayer === "p1" ? (currPlayer = "p2") : (currPlayer = "p1");
-  htmlBoard.classList.toggle("player1Turn");
-  htmlBoard.classList.toggle("player2Turn");
+
+    /**go to next turn after animation is complete */
+    const callback = () => {
+        allowClick = true;
+
+        // check for win
+        if (checkForWin(y, x)) {
+            return endGame(`Player ${currPlayer} won!`);
+        }
+        // check for tie
+        // TODO: check if all cells in board are filled; if so call, call endGame
+        if (board.every((cell) => cell === "")) {
+            endGame(`Tie Game! Start New Game`);
+            console.log("tie");
+        }
+        // switch players
+        // TODO: switch currPlayer 1 <-> 2
+        const htmlBoard = document.querySelector("#column-top");
+        currPlayer === "1" ? (currPlayer = "2") : (currPlayer = "1");
+        displayPlayer.innerText = currPlayer;
+        htmlBoard.classList.toggle("player1Turn");
+        htmlBoard.classList.toggle("player2Turn");
+    }
+
+    // place piece in board and add to HTML table
+    // TODO: add line to update in-memory board
+    board[y][x] = currPlayer;
+    placeInTable(y, x, callback);
+
 }
 
 /** checkForWin: check aroung the placed chip for 3 more chips of same player*/
 
 function checkForWin(y, x) {
-  const _win = {
-    upLeft: (y, x) => {
-      let matches = 0;
-      y--;
-      x--;
-      while (testMatch(y, x)) {
-        y--;
-        x--;
-        matches++;
-      }
+    const _win = {
+        upLeft: (y, x) => {
+            let matches = 0;
+            y--;
+            x--;
+            while (testMatch(y, x)) {
+                y--;
+                x--;
+                matches++;
+            }
 
-      return matches;
-    },
+            return matches;
+        },
 
-    left: (y, x) => {
-      let matches = 0;
-      x--;
-      while (testMatch(y, x)) {
-        x--;
-        matches++;
-      }
-      return matches;
-    },
+        left: (y, x) => {
+            let matches = 0;
+            x--;
+            while (testMatch(y, x)) {
+                x--;
+                matches++;
+            }
+            return matches;
+        },
 
-    downLeft: (y, x) => {
-      let matches = 0;
-      y++;
-      x--;
-      while (testMatch(y, x)) {
-        y++;
-        x--;
-        matches++;
-      }
-      return matches;
-    },
+        downLeft: (y, x) => {
+            let matches = 0;
+            y++;
+            x--;
+            while (testMatch(y, x)) {
+                y++;
+                x--;
+                matches++;
+            }
+            return matches;
+        },
 
-    down: (y, x) => {
-      let matches = 0;
-      y++;
-      while (testMatch(y, x)) {
-        y++;
-        matches++;
-      }
-      return matches;
-    },
+        down: (y, x) => {
+            let matches = 0;
+            y++;
+            while (testMatch(y, x)) {
+                y++;
+                matches++;
+            }
+            return matches;
+        },
 
-    downRight: (y, x) => {
-      let matches = 0;
-      y++;
-      x++;
-      while (testMatch(y, x)) {
-        y++;
-        x++;
-        matches++;
-      }
-      return matches;
-    },
+        downRight: (y, x) => {
+            let matches = 0;
+            y++;
+            x++;
+            while (testMatch(y, x)) {
+                y++;
+                x++;
+                matches++;
+            }
+            return matches;
+        },
 
-    right: (y, x) => {
-      let matches = 0;
-      x++;
-      while (testMatch(y, x)) {
-        x++;
-        matches++;
-      }
-      return matches;
-    },
+        right: (y, x) => {
+            let matches = 0;
+            x++;
+            while (testMatch(y, x)) {
+                x++;
+                matches++;
+            }
+            return matches;
+        },
 
-    upRight: (y, x) => {
-      let matches = 0;
-      y--;
-      x++;
-      while (testMatch(y, x)) {
-        y--;
-        x++;
-        matches++;
-      }
-      return matches;
-    },
-  };
+        upRight: (y, x) => {
+            let matches = 0;
+            y--;
+            x++;
+            while (testMatch(y, x)) {
+                y--;
+                x++;
+                matches++;
+            }
+            return matches;
+        },
+    };
 
-  /**testMatch checks if coordinates are on the board and if chip is assigned to currentPlayer */
-  const testMatch = (y, x) => {
+    /**testMatch checks if coordinates are on the board and if chip is assigned to currentPlayer */
+    const testMatch = (y, x) => {
+        if (
+            y > 0 &&
+            y < HEIGHT &&
+            x > 0 &&
+            x < WIDTH &&
+            board[y][x] === currPlayer
+        ) {
+            return true;
+        }
+        return false;
+    };
+
+    let horizontalMatches = _win.left(y, x) + _win.right(y, x);
+    let verticalMatches = _win.down(y, x);
+    let rightDiagnolMatches = _win.downLeft(y, x) + _win.upRight(y, x);
+    let leftDiagnolMatches = _win.upLeft(y, x) + _win.downRight(y, x);
+
     if (
-      y > 0 &&
-      y < HEIGHT &&
-      x > 0 &&
-      x < WIDTH &&
-      board[y][x] === currPlayer
+        horizontalMatches >= 3 ||
+        verticalMatches >= 3 ||
+        rightDiagnolMatches >= 3 ||
+        leftDiagnolMatches >= 3
     ) {
-      return true;
+        return true;
     }
-    return false;
-  };
-
-  let horizontalMatches = _win.left(y, x) + _win.right(y, x);
-  let verticalMatches = _win.down(y, x);
-  let rightDiagnolMatches = _win.downLeft(y, x) + _win.upRight(y, x);
-  let leftDiagnolMatches = _win.upLeft(y, x) + _win.downRight(y, x);
-
-  if (
-    horizontalMatches >= 3 ||
-    verticalMatches >= 3 ||
-    rightDiagnolMatches >= 3 ||
-    leftDiagnolMatches >= 3
-  ) {
-    return true;
-  }
 }
 
 makeBoard();
@@ -282,12 +278,12 @@ makeHtmlBoard();
 
 /**reset html and start new game */
 function newGame(evt) {
-  evt.preventDefault();
-  const htmlBoard = document.querySelector("#board");
-  htmlBoard.innerHTML = "";
-  htmlBoard.classList.remove("player2Turn");
-  currPlayer = "p1";
-  allowClick = true;
-  makeBoard();
-  makeHtmlBoard();
+    evt.preventDefault();
+    const htmlBoard = document.querySelector("#board");
+    htmlBoard.innerHTML = "";
+    htmlBoard.classList.remove("player2Turn");
+    currPlayer = "1";
+    allowClick = true;
+    makeBoard();
+    makeHtmlBoard();
 }
